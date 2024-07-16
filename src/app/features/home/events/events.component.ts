a2dc43043dad9e1f299e900bc2ab70a4823340d0
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-07-16 11:03:35 +0200

    feat: Add event list to events component

--- src/app/features/home/events/events.component.ts
+++ src/app/features/home/events/events.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { EssentialComponent } from '../../../core/components/essentialComponent';
 import { AuthService } from '../../../core/services/auth.service';
 import { UserTypes } from '../../../core/models/users';
@@ -12,16 +12,21 @@ import { EventsService } from '../../../core/services/events.service';
   styleUrl: './events.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventsComponent extends EssentialComponent {
+export class EventsComponent extends EssentialComponent implements OnInit {
   authService = inject(AuthService);
   eventsService = inject(EventsService);
   userTypes = UserTypes;
   formConfig = EventForm;
   currentUserType = this.authService.currentUser.type;
+  events$ = this.eventsService.events$
+
+  ngOnInit(){
+    this.eventsService.getEventsList()
+  }
 
   handleCreation(event: Partial<GameEvent>) {
-    this.takeUntilDestroy(
+
       this.eventsService.createEvent(event)
-    ).subscribe();
+
   }
 }
